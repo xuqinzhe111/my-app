68716b1e7844c2ac02e3f1d2cb8ae0e6712b090a
Gabriel <1021453395@qq.com>
2023-08-30 00:09:29 +1000

    init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
+type RootLayoutProps = {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  )
+}
